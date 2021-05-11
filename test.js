f1f6dd5cb8b589ceccae30a7ff1e0b0230ce3339
Masa <katsukatsu.masa.0522@gmail.com>
2021-05-11 17:36:56 +0900

    apply,call,bind

--- test.js
+++ test.js
@@ -1,43 +1,22 @@
+function greet(id1,id2,id3) {
+    console.log(arguments)
+    let slicedArray = [].slice.call(arguments);
+    let hi = `Hi,${this.name}`
+    console.log(slicedArray);
+}
+
+let obj = {name: "Tom"};
+
+const arry = [1,2,3,4];
+console.log(Math.min.apply(null,arry))
+
 let myObj = {
-    id: 2,
-    printId(){
-        console.log(this.id);
+    id: 42,
+    print() {
+
+        setTimeout( () => {
+            console.log(this.id);
+        }, 1000);
     }
 }
-
-myObj.printId();
-
-const sayFoo = function(){
-    console.log(this['foo'])
-}
-
-foo = 'foo';
-
-const mySecondObj = {
-    foo: "I'min the obj",
-    sayFoo
-}
-
-sayFoo()
-mySecondObj.sayFoo()
-
-function MyObj(id){
-    this.id = id;
-}
-
-MyObj.prototype.printId = function(id){
-    console.log(this.id);
-}
-
-// const newInstance = new MyObj(5);
-
-class MyClass {
-    constructor(id){
-        this.id = id;
-    }
-    
-    printId(id){
-        console.log(this.id)
-    }
-}
-const newInstance = new MyObj(5);
+myObj.print()
